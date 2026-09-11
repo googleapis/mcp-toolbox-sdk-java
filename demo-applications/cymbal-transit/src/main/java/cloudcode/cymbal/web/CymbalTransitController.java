3ce064c8807d59ad58bc768ea8e3da49e2fc6c4a
Stenal P Jolly <stenalpjolly@google.com>
2026-09-11 21:00:16 +0530

    refactor(demo): remove redundant main method from CymbalTransitController

--- demo-applications/cymbal-transit/src/main/java/cloudcode/cymbal/web/CymbalTransitController.java
+++ demo-applications/cymbal-transit/src/main/java/cloudcode/cymbal/web/CymbalTransitController.java
@@ -16,7 +16,6 @@
 
 package cloudcode.cymbal.web;
 
-import cloudcode.cymbal.CymbalTransitApplication;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.IdTokenProvider;
 import com.google.cloud.mcp.McpToolboxClient;
@@ -40,7 +39,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
@@ -54,10 +52,6 @@ public class CymbalTransitController {
   @GetMapping("/")
   public String index() {
     return "index";
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(CymbalTransitApplication.class, args);
   }
 }
 
